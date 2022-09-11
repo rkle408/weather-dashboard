286bfc6b5000c60298b3b50b1ff02f6b4503c0be
Rosebud <rkle408@gmail.com>
2022-09-10 19:29:24 -0700

    updated notes of what is needed

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 // TO DO:
 // Need to fetch weather API
 // Need to do local storage for user's search history
+// Need a search bar
+// Need list of city history
+
 
 // link API using
-var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&exclude={part}&appid={90f20119a63b80bc6e3ec3b202bae4ee}";
+var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={90f20119a63b80bc6e3ec3b202bae4ee}";
+
+// Need to figure out how to get uer's desired input in the lat and long in the URL above
 
 // Fetch API using the browswer fetch method (no need to worry about linking jquery with AJAX)
 fetch(openWeatherUrl)
