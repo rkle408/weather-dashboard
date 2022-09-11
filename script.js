d062213b323d699f58946f549689786a5e3ece86
Rosebud <rkle408@gmail.com>
2022-09-10 22:11:33 -0700

    added skeleton for weather area

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 // TO DO:
 // Need to fetch weather API
+// NEED TO RENDER THE WEATHER!!!
 // Need to do local storage for user's search history - done
 // Need a functional search bar
 // Need list of city history
@@ -13,6 +14,7 @@ var timeEl = $('.timeNow');
 var userCities = $('#citySearch');
 var searchButton = $('#citySearchButton');
 var searchHistory= $('#searchHistoryList');
+var cityName = $('city-name')
 
 function displayTime () {
     var timeNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -36,18 +38,17 @@ searchButton.on('click', function (event) {
 var storedCities = localStorage.getItem("searchedCity");
 searchHistory.text(storedCities);
 
-// link API using
-var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={90f20119a63b80bc6e3ec3b202bae4ee}";
+// Need function to get the weather and to display data:
+    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=90f20119a63b80bc6e3ec3b202bae4ee";
 
-// Need to figure out how to get uer's desired input in the lat and long in the URL above
+    // Need to figure out how to get uer's desired input in the lat and long in the URL above
 
-// Fetch API using the browswer fetch method (no need to worry about linking jquery with AJAX)
-fetch(openWeatherUrl)
-    .then(function (response) {
-        return response.json();
-      })
-      // Need to convert
-      .then(function (data) {
-        console.log(data)
-      });
-
+    // Fetch API using the browswer fetch method (no need to worry about linking jquery with AJAX)
+    fetch(openWeatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        // Need to convert
+        .then(function (data) {
+            console.log(data)
+        });
