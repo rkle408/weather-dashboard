8c1db2119524136c417ed94fbf8f69eb961c335e
Rosebud <rkle408@gmail.com>
2022-09-10 20:42:00 -0700

    added local storage for searched cities

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 
 // TO DO:
 // Need to fetch weather API
-// Need to do local storage for user's search history
+// Need to do local storage for user's search history - done
 // Need a functional search bar
 // Need list of city history
 // Create buttons for previous cities in .searchHistoryList
 
 // Add time to header
 var timeEl = $('.timeNow');
-var userCities = $('.citySearch');
+var userCities = $('#citySearch');
+var searchButton = $('#citySearchButton');
 
 function displayTime () {
     var timeNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -22,6 +23,12 @@ function displayTime () {
 // Declare time function
 setInterval(displayTime, 1000);
 
+searchButton.on('click', function (event) { 
+    event.preventDefault();
+
+    console.log(userCities.val());
+    localStorage.setItem("searchedCity", userCities.val());
+});
 
 // link API using
 var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={90f20119a63b80bc6e3ec3b202bae4ee}";
