65d18881fe8c4c0e557db44ae15d392bb7440507
Rosebud <rkle408@gmail.com>
2022-09-11 16:05:08 -0700

    moved code around, attempt to isolate lat and long

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var timeEl = $('.timeNow');
 var userCities = $('#citySearch');
 var searchButton = $('#citySearchButton');
 var searchHistory= $('#searchHistoryList');
-var cityName = $('#city-name')
+var cityName =$('#city-name');
 
 function displayTime () {
     var timeNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -28,20 +28,12 @@ setInterval(displayTime, 1000);
 
 // Search button saves to local storage
 // NEED TO FIGURE OUT HOW TO SAVE ALL INDIVIDUAL SEARCHES AND DISPLAY AS BUTTONS
-searchButton.on('click', function (event) { 
-    event.preventDefault();
-
-    console.log(userCities.val());
-    localStorage.setItem("searchedCity", userCities.val());
-});
-
-var storedCities = localStorage.getItem("searchedCity");
-searchHistory.text(storedCities);
 
 // Need function to get the weather and to display data:
-    var openWeatherUrl = "90f20119a63b80bc6e3ec3b202bae4ee";
+function getCityData() {
+    var openWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+userCities.val()+"&appid=90f20119a63b80bc6e3ec3b202bae4ee";
 
-    // Need to figure out how to get uer's desired input in the lat and long in the URL above
+    // Need to figure out how to get user's desired input in the lat and long in the URL above
 
     // Fetch API using the browswer fetch method (no need to worry about linking jquery with AJAX)
     fetch(openWeatherUrl)
@@ -50,5 +42,19 @@ searchHistory.text(storedCities);
         })
         // Need to convert
         .then(function (data) {
-            console.log(data)
+            console.log(data);
         });
+    };
+
+searchButton.on('click', function (event) { 
+    event.preventDefault();
+
+    console.log(userCities.val());
+    localStorage.setItem("searchedCity", userCities.val());
+    
+    // Need to make sure API is fetched
+    getCityData(userCities);
+});
+
+var storedCities = localStorage.getItem("searchedCity");
+searchHistory.text(storedCities);
